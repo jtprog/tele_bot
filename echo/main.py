07b180360bd3a09e134c6da3b4d68078c82cdc93
Vladimir <volkvid@yandex.ru>
2019-04-05 22:31:28 +0300

    Добавить команды help и time

--- echo/main.py
+++ echo/main.py
@@ -1,3 +1,6 @@
+from subprocess import Popen
+from subprocess import PIPE
+
 from telegram import Bot
 from telegram import Update
 from telegram.ext import Updater
@@ -13,6 +16,32 @@ def do_start(bot: Bot, update: Update):
     bot.send_message(
         chat_id=update.message.chat_id,
         text="Привет! Отправь мне что-нибудь",
+    )
+
+
+def do_help(bot: Bot, update: Update):
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Это учебный бот\n\n"
+             "Список доступных команд есть в меню\n\n"
+             "Так же я отвечую на любое сообщение",
+    )
+
+
+def do_time(bot: Bot, update: Update):
+    """ Узнать серверное время
+    """
+    process = Popen(["date"], stdout=PIPE)
+    text, error = process.communicate()
+    # Может произойти ошибка вызова процесса (код возврата не 0)
+    if error:
+        text = "Произошла ошибка, время неизвестно"
+    else:
+        # Декодировать ответ команды из процесса
+        text = text.decode("utf-8")
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=text,
     )
 
 
@@ -35,9 +64,13 @@ def main():
     )
 
     start_handler = CommandHandler("start", do_start)
+    help_handler = CommandHandler("help", do_help)
+    time_handler = CommandHandler("time", do_time)
     message_handler = MessageHandler(Filters.text, do_echo)
 
     updater.dispatcher.add_handler(start_handler)
+    updater.dispatcher.add_handler(help_handler)
+    updater.dispatcher.add_handler(time_handler)
     updater.dispatcher.add_handler(message_handler)
 
     # Начать обработку входящих сообщений
