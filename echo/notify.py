135df32b75f415541190c18baa93bd7bc85cf67a
Vladimir <volkvid@yandex.ru>
2019-05-09 01:27:07 +0300

    Добавить возможность отправлять уведомление о курсе валют в виде картинки

--- echo/notify.py
+++ echo/notify.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 from logging import getLogger
 
 from telegram import Bot
@@ -6,23 +8,39 @@ from telegram.utils.request import Request
 from apis.bittrex import BittrexClient
 from apis.bittrex import BittrexError
 from echo.config import load_config
+from echo.draw import drawer
+from echo.utils import save_image
 
 
 logger = getLogger(__name__)
 
 
-NOTIFY_PAIR = "USD-BTC"
-NOTIFY_USER_ID = 720951086
-
-
 def main():
+    parser = argparse.ArgumentParser()
+    # Оба аргумента опциональные, поэтому они начинаются с --
+    parser.add_argument("--image", help="Отправить сообщение с картинкой", action="store_true")
+    parser.add_argument("--text", help="Отправить сообщение с текстом", action="store_true")
+    args = parser.parse_args()
+
+    # Нельзя указать оба сразу
+    if args.image and args.text:
+        logger.error("Нельзя указать одновременно `--image` и `--text`")
+        sys.exit(1)
+
+    # Если ничего не указано, то считаем что это --text
+    if not any([args.image, args.text]):
+        args.text = True
+
+    config = load_config()
+
     client = BittrexClient()
 
     try:
-        current_price = client.get_last_price(pair=NOTIFY_PAIR)
-        message = "{} = {}".format(NOTIFY_PAIR, current_price)
+        current_price = client.get_last_price(pair=config.NOTIFY_PAIR)
+        message = "{} = {}".format(config.NOTIFY_PAIR, current_price)
     except BittrexError:
         logger.error("BittrexError")
+        current_price = None
         message = "Произошла ошибка"
 
     config = load_config()
@@ -48,6 +66,27 @@ def main():
         text=message,
     )
     logger.info('Success: %s', message)
+
+    if args.text:
+        bot.send_message(
+            chat_id=config.NOTIFY_USER_ID,
+            text=message,
+        )
+        logger.info("текстовое сообщение отправлено")
+    if args.image:
+        if current_price:
+            text = [config.NOTIFY_PAIR, current_price]
+        else:
+            text = [config.NOTIFY_PAIR, message]
+
+        img = drawer(text=text)
+        photo = save_image(img=img)
+        bot.send_photo(
+            chat_id=config.NOTIFY_USER_ID,
+            photo=photo,
+            caption='Подпись под фото',
+        )
+        logger.info("картинка отправлена")
 
 
 if __name__ == '__main__':
