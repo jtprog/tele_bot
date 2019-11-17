177cc9e1d98c9fbf15c64bc023cf97f9465731df
Vladimir <volkvid@yandex.ru>
2019-11-17 15:39:29 +0300

    Добавить фильтр по цене

--- avito_parser_django/avito/aparser/admin.py
+++ avito_parser_django/avito/aparser/admin.py
@@ -4,8 +4,48 @@ from .forms import ProductForm
 from .models import Product
 
 
+PRICE_FILTER_STEPS = 10
+
+
+class PriceFilter(admin.SimpleListFilter):
+    title = 'Цена'
+    parameter_name = 'price'
+
+    def lookups(self, request, model_admin):
+        # Вытащить полный список цен
+        prices = [c.price for c in model_admin.model.objects.all()]
+        prices = filter(None, prices)
+
+        # TODO: найти "кластера цен", то есть такие интервалы, внутри которых точно есть продукты!
+
+        # Побить его на 10 интервалов
+        max_price = max(prices)
+        chunk = int(max_price / PRICE_FILTER_STEPS)
+        print(f'max_price = {max_price}, chunk = {chunk}')
+
+        intervals = [
+            (f'{chunk * i},{chunk * (i + 1)}', f'{chunk * i} - {chunk * (i + 1)}')
+            for i in range(PRICE_FILTER_STEPS)
+        ]
+        return intervals
+
+    def queryset(self, request, queryset):
+        choice = self.value() or ''
+        if not choice:
+            return queryset
+        choice = choice.split(',')
+        if not len(choice) == 2:
+            return queryset
+        price_from, price_to = choice
+        return queryset.distinct().filter(price__gte=price_from, price__lt=price_to)
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'price', 'currency', 'published_date', 'url')
-    list_filter = ('currency', 'published_date')
+    list_filter = (
+        'currency',
+        'published_date',
+        PriceFilter,
+    )
     form = ProductForm
